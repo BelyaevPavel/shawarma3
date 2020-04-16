19cd86a44a83d56d2d85b380dd255ce6dd335f89
Palov <gbelbih@gmail.com>
2020-04-16 22:57:18 +0500

    Shawarma site removed.

--- shawarma_site/customer_interface/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CustomerInterfaceConfig(AppConfig):
-    name = 'customer_interface'
--- shawarma_site/customer_interface/static/customer_interface/js/menu_handlers.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by paul on 12.04.20.
- */
-$(document).ready(function () {
-    $('.block-title').click(function (event) {
-        $(this).toggleClass('active').next().slideToggle(300);
-    });
-});
